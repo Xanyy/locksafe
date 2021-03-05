69841020506474482a644e7f9ac73a0f673c7194
Sinead Cowhig <sineadcowhig@gmail.com>
2021-03-05 01:00:11 +0000

    Changes to login file

--- app/src/main/java/com/example/locksafe/Login.java
+++ app/src/main/java/com/example/locksafe/Login.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Login extends AppCompatActivity {
+    private TextInputEditText txtEdit_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
